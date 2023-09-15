419dc7838772606577689e1f4540176b0a060d7c
beh-karimi <beh.karimi@proton.me>
2023-09-15 21:56:47 +0330

    made sending and listening for message async using tokio

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use dh_chat::{utils, net};
 
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("choose operation mode:\n\
              c  client mode\n\
              s  server mode");
 
     let ans = utils::get_inp().unwrap();
 
-    if ans=="c" { net::client_mode(); }
-    else if ans=="s" { net::make_server(); }
+    if ans=="c" { net::client_mode().await; }
+    else if ans=="s" { net::make_server().await; }
 }
--- src/net.rs
+++ src/net.rs
@@ -3,6 +3,7 @@ use std::{
     net::{TcpListener, TcpStream},
     io::BufReader,
     io::prelude::*,
+    sync::Arc,
 };
 
 use crate::{
@@ -10,7 +11,8 @@ use crate::{
     crypt::dh::{KeyPair, self},
     crypt::{self, decrypt, encrypt},
 };
-pub fn make_server() {
+
+pub async fn make_server() {
     let key = match utils::read_key("server.key") {
         Ok(k) => k,
         Err(AppError::Io(_)) => {
@@ -33,11 +35,11 @@ pub fn make_server() {
     let listener = TcpListener::bind("0.0.0.0:25565").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_server_connection(stream, &key);
+        handle_server_connection(stream, &key).await;
     }
 }
 
-fn handle_server_connection(mut stream: TcpStream, key: &KeyPair) {
+async fn handle_server_connection(mut stream: TcpStream, key: &KeyPair) {
     let pubkey_string = format!("{}\n{}\n{}\n", key.public, key.modulus, key.gen);
     stream.write_all(pubkey_string.as_bytes()).unwrap();
 
@@ -49,36 +51,17 @@ fn handle_server_connection(mut stream: TcpStream, key: &KeyPair) {
     let shared_key = dh::gen_common_key(key.modulus, key.private, cl_pubkey);
     println!("shared key established.");
 
-    loop {
-        let inp = utils::get_inp().unwrap();
-        let mut inp_len = inp.len();
-        let mut msg = vec![];
-        while inp_len >= 255 {
-            inp_len -= 255;
-            msg.push(255u8);
-        }
-        msg.push(inp_len as u8);
-        let mut enc = encrypt(&shared_key, &inp);
-        msg.append(&mut enc);
-        stream.write_all(&msg).unwrap();
+    let skey_ref = Arc::new(shared_key);
 
-        let mut inc_msg_len: u64 = 0;
-        let mut len_buf = [0u8];
-        loop {
-            stream.read_exact(&mut len_buf).unwrap();
-            inc_msg_len += len_buf[0] as u64;
-            if len_buf[0]!=255 {break;}
-        }
-
-        let mut inc_msg = vec![0u8; inc_msg_len as usize];
-        stream.read_exact(&mut inc_msg).unwrap();
-        let inc_msg = decrypt(&shared_key, &inc_msg);
-        println!("{}", inc_msg.trim());
-
-    }
+    let mut stream_cp = stream.try_clone().unwrap();
+    let key_c = Arc::clone(&skey_ref);
+    tokio::spawn(async move {
+        receive_msg(&mut stream, &key_c).await;
+    });
+    send_msg(&mut stream_cp, &skey_ref).await;
 }
 
-pub fn client_mode() {
+pub async fn client_mode() {
     println!("address to connect to (ip:port)");
     let addr = utils::get_inp().unwrap();
     let mut stream = TcpStream::connect(addr).unwrap();
@@ -88,12 +71,26 @@ pub fn client_mode() {
     for (i,line) in buf.lines().take(3).enumerate() {
         key_details[i] = line.unwrap().trim().parse().unwrap();
     }
+
     let key = crypt::dh::gen_key_pair(key_details[1], key_details[2]);
     let key_str = key.public.to_string()+"\n";
     stream.write_all(key_str.as_bytes()).unwrap();
     let shared_key = crypt::dh::gen_common_key(key.modulus, key.private, key_details[0]);
     println!("got the shared key");
 
+    let skey_ref = Arc::new(shared_key);
+
+    let mut stream_cp = stream.try_clone().unwrap();
+    let key_c = Arc::clone(&skey_ref);
+    tokio::spawn(async move {
+        receive_msg(&mut stream, &key_c).await;
+    });
+    send_msg(&mut stream_cp, &skey_ref).await;
+
+
+}
+
+async fn receive_msg(stream: &mut TcpStream, shared_key: &[u8]) {
     loop {
         let mut inc_msg_len: u64 = 0;
         let mut len_buf = [0u8];
@@ -105,9 +102,13 @@ pub fn client_mode() {
 
         let mut inc_msg = vec![0u8; inc_msg_len as usize];
         stream.read_exact(&mut inc_msg).unwrap();
-        let inc_msg = decrypt(&shared_key, &inc_msg);
+        let inc_msg = decrypt(shared_key, &inc_msg);
         println!("{}", inc_msg.trim());
+    }
+}
 
+async fn send_msg(stream: &mut TcpStream, shared_key: &[u8]) {
+    loop {
         let inp = utils::get_inp().unwrap();
         let mut inp_len = inp.len();
         let mut msg = vec![];
@@ -116,9 +117,8 @@ pub fn client_mode() {
             msg.push(255u8);
         }
         msg.push(inp_len as u8);
-        let mut enc = encrypt(&shared_key, &inp);
+        let mut enc = encrypt(shared_key, &inp);
         msg.append(&mut enc);
         stream.write_all(&msg).unwrap();
     }
-
 }
